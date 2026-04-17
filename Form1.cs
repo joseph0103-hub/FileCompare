f3f9be04d15ed1ef4b18be1b5d8535692a0f50d6
23017065@suwon.ac.kr <23017065@suwon.ac.kr>
2026-04-17 14:34:24 +0900

    과제2 코딩과 디버깅 진행중  - 폴더 선택 기능  - 파일 리스트 기능 구현(색상 구분 표시)

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace FileCompare
@@ -7,6 +11,117 @@ namespace FileCompare
         public Form1()
         {
             InitializeComponent();
+            InitializeListViews();
+        }
+
+        private void InitializeListViews()
+        {
+            ConfigureListView(lvwLeftDir);
+            ConfigureListView(lvwRightDir);
+        }
+
+        private void ConfigureListView(ListView listView)
+        {
+            listView.View = View.Details;
+            listView.FullRowSelect = true;
+            listView.GridLines = true;
+            listView.HideSelection = false;
+        }
+
+        private void btnLeftDir_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "왼쪽 폴더를 선택하세요.";
+                dialog.ShowNewFolderButton = false;
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtLeftDir.Text = dialog.SelectedPath;
+                    RefreshBothLists();
+                }
+            }
+        }
+
+        private void btnRightDir_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "오른쪽 폴더를 선택하세요.";
+                dialog.ShowNewFolderButton = false;
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtRightDir.Text = dialog.SelectedPath;
+                    RefreshBothLists();
+                }
+            }
+        }
+
+        private void RefreshBothLists()
+        {
+            PopulateListView(lvwLeftDir, txtLeftDir.Text, txtRightDir.Text);
+            PopulateListView(lvwRightDir, txtRightDir.Text, txtLeftDir.Text);
+        }
+
+        private void PopulateListView(ListView targetListView, string sourceDir, string compareDir)
+        {
+            targetListView.Items.Clear();
+
+            if (string.IsNullOrWhiteSpace(sourceDir) || !Directory.Exists(sourceDir))
+            {
+                return;
+            }
+
+            var files = new DirectoryInfo(sourceDir)
+                .GetFiles("*", SearchOption.TopDirectoryOnly)
+                .OrderBy(f => f.Name, StringComparer.CurrentCultureIgnoreCase)
+                .ToArray();
+
+            foreach (var file in files)
+            {
+                var item = new ListViewItem(file.Name);
+                item.SubItems.Add(ToDisplaySize(file.Length));
+                item.SubItems.Add(file.LastWriteTime.ToString("yyyy-MM-dd tt h:mm"));
+
+                item.ForeColor = GetFileColor(file, compareDir);
+                targetListView.Items.Add(item);
+            }
+        }
+
+        private Color GetFileColor(FileInfo sourceFile, string compareDir)
+        {
+            if (string.IsNullOrWhiteSpace(compareDir) || !Directory.Exists(compareDir))
+            {
+                return Color.Black;
+            }
+
+            string targetPath = Path.Combine(compareDir, sourceFile.Name);
+
+            if (!File.Exists(targetPath))
+            {
+                return Color.Red;
+            }
+
+            var targetFile = new FileInfo(targetPath);
+
+            if (sourceFile.LastWriteTime > targetFile.LastWriteTime)
+            {
+                return Color.Blue;
+            }
+
+            if (sourceFile.LastWriteTime < targetFile.LastWriteTime)
+            {
+                return Color.DarkGray;
+            }
+
+            return Color.Black;
+        }
+
+        private string ToDisplaySize(long length)
+        {
+            long kb = (long)Math.Ceiling(length / 1024.0);
+            return kb + " KB";
         }
     }
 }
